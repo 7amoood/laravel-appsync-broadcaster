b23dc667b62ac1ea3bc028407520a6245d78b9d5
= <=>
2025-08-28 04:20:46 +0300

    Refactor config merging and publishing paths in AppSyncBroadcastServiceProvider

--- src/AppSyncBroadcastServiceProvider.php
+++ src/AppSyncBroadcastServiceProvider.php
@@ -8,20 +8,17 @@ class AppSyncBroadcastServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        // Only merge config if not published
-        if (! file_exists($this->app->configPath('broadcasting/appsync.php'))) {
-            $this->mergeConfigFrom(
-                __DIR__ . '/../config/appsync.php',
-                'broadcasting.connections.appsync'
-            );
-        }
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/appsync.php',
+            'broadcasting.connections.appsync'
+        );
     }
 
     public function boot(BroadcastManager $broadcastManager)
     {
         // Publish config file
         $this->publishes([
-            __DIR__ . '/../config/appsync.php' => $this->app->configPath('broadcasting/appsync.php'),
+            __DIR__ . '/../config/appsync.php' => $this->app->configPath('appsync.php'),
         ], 'appsync-config');
 
         $broadcastManager->extend('appsync', function ($app, $config) {
